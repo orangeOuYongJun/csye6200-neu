ce4eba69484c450dd40f1cd4c94b18573dfb150c
Yongjun Ou <ou.yo@husky.neu.edu>
2020-01-07 17:37:01 -0500

    1.create hello world

---
+++ class_01/src/edu/neu/csye6200/c01/Driver.java
@@ -0,0 +1,12 @@
+package edu.neu.csye6200.c01;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
+
+
